53662e918015f9b7868804ba4bf0fbc14ea12c5c
Odysimus <theborg989@gmail.com>
2014-08-12 19:01:04 -0700

    Additions
    Added more files, current authorized keys file, shell config, and vim files

---
+++ configs/etcprofiled/custom.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+alias Ls='ls -lah'
+alias update-grub='grub2-mkconfig -o /boot/grub2/grub.cfg'
+alias du-1='du -h --max-depth=1'
+alias update-all-the-things='yum update && update-grub'
---
+++ configs/vim/.vimrc
@@ -0,0 +1,5 @@
+set spellfile=~/.vim/spellfile.add
+set spell
+nnoremap <f8> :tabp<cr>
+nnoremap <f9> :tabn<cr>
+nnoremap <f7> :tabe<space>
